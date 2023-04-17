35cde90cb8b023f0ee905a4b057d4c8585f381ba
lyx2003 <1095264563@qq.com>
2023-04-17 21:01:32 +0800

    fix_bug

--- src/test2.hpp
+++ src/test2.hpp
@@ -7,21 +7,45 @@
 #include <iostream>
 #include <format>
 #include <fstream>
+#include <vector>
+#include <list>
+#include <memory>
 
 const double PI = 3.1415926;
 const double maxNums = 4194304.0;
 using gene = std::array<double, 22>;
-const double crossRate = 0.9;
-const double mutateRate = 0.1;
-const int populationSize = 10;
-const int maxGeneration = 100;
+const double crossRate = 0.3;
+const double mutateRate = 0.05;
+const int populationSize = 512;
+const int maxGeneration = 500;
 
 std::random_device seed;
 std::mt19937 get(seed());
 
+class individual;
+class population;
+std::list<individual *> allAllocatedMemory; // 用于释放内存
+std::ofstream writeFile{"gene.txt", std::ios::out &std::ios::trunc &std::ios::app};
+
 double targetFunction(double x)
 {
     return (x * std::sin(PI * 10 * x) + 2.0);
+}
+
+void save(gene x)
+{
+    if (writeFile)
+    {
+        for (auto &e : x)
+        {
+            writeFile << e << " ";
+        }
+        writeFile << std::endl;
+    }
+    else
+    {
+        std::cerr << "can not open writefile" << std::endl;
+    }
 }
 
 class individual
@@ -51,7 +75,7 @@ private:
         std::uniform_int_distribution<> dist(0, 1);
         for (auto &e : x)
         {
-            e = dist(get) % 2;
+            e = dist(get);
         }
     }
 
@@ -60,93 +84,109 @@ private:
         fitness = targetFunction(getDecode());
     }
 
-    void cross(individual &other, int point)
+    std::pair<std::shared_ptr<individual>, std::shared_ptr<individual>> cross(std::shared_ptr<individual> other, int point)
     {
+        std::shared_ptr<individual> temp1 = std::make_shared<individual>();
+        std::shared_ptr<individual> temp2 = std::make_shared<individual>();
+        temp1->x = this->x;
+        temp2->x = temp2->x;
+        writeFile << "cross over and cross location is " << point << std::endl;
+        //save(temp1->x);
+        //save(temp2->x);
         for (int i = point; i < 22; ++i)
         {
-            std::swap(x[i], other.x[i]);
+            std::swap(temp1->x[i], temp2->x[i]);
         }
+        //save(temp1->x);
+        //save(temp2->x);
+        return std::make_pair(temp1, temp2);
     }
 
-    void mutate()
+    std::shared_ptr<individual> mutate()
     {
         std::uniform_int_distribution<> dist(0, 21);
         int r = dist(get);
-        double tempFitness = fitness;
-        x[r] = !x[r];
-        this->getFitness();
-        if (tempFitness > fitness)
-        {
-            // std::cout << "fitness less" << std::endl;
-            x[r] = !x[r];
-            this->fitness = tempFitness;
-        }
-        // else
-        // {
-        //     std::cout << "fitness better" << std::endl;
-        // }
+        writeFile << "mutateOver and mutation location is " << r << std::endl;
+        std::shared_ptr<individual> temp = std::make_shared<individual>();
+        temp->x = this->x;
+        temp->x[r] = !temp->x[r];
+        //save(this->x);
+        //save(temp->x);
+        return temp;
     }
 };
 
 class population
 {
 public:
-    std::array<individual, populationSize> allPopulation;
+    std::array<std::shared_ptr<individual>, populationSize> selectedPopulation;
+    std::vector<std::shared_ptr<individual>> currentPopulation;
 
 public:
     population()
     {
-        for (auto &e : allPopulation)
+        for (auto &e : selectedPopulation)
         {
-            e.initIndividual();
+            e = std::make_shared<individual>();
+            e->initIndividual();
+            currentPopulation.push_back(e);
         }
     }
 
     void calculateFitness()
     {
-        for (auto &e : allPopulation)
+        for (auto &e : currentPopulation)
         {
-            e.getFitness();
+            e->getFitness();
         }
     }
 
     void select()
     {
         double fitnessSum = 0.0;
-        for (auto &e : allPopulation)
+        for (auto &e : currentPopulation)
         {
-            fitnessSum += e.fitness;
+            fitnessSum += e->fitness;
         }
         std::uniform_real_distribution<double> dis(0.0, 1.0);
         for (int i = 0; i < populationSize; ++i)
         {
             double r = dis(get) * fitnessSum;
             double sum = 0;
-            for (int j = 0; j < populationSize; ++j)
+            for (int j = 0; j < currentPopulation.size(); ++j)
             {
-                sum += allPopulation[j].fitness;
+                sum += currentPopulation[j]->fitness;
                 if (sum >= r)
                 {
-                    allPopulation[i] = allPopulation[j];
+                    selectedPopulation[i] = currentPopulation[j];
                     break;
                 }
             }
         }
+        currentPopulation.clear();
+        std::for_each(selectedPopulation.begin(), selectedPopulation.end(), [&](std::shared_ptr<individual> e)
+                      { currentPopulation.push_back(e); });
     }
 
     void crossOver()
     {
-        std::uniform_int_distribution<> dId(0, populationSize - 1);
-        std::uniform_int_distribution<> dPoint(0, 21);
+        size_t size = currentPopulation.size();
+        std::uniform_int_distribution<> dId(0, size - 1);
+        std::uniform_int_distribution<> dPoint(1, 21);
         std::uniform_real_distribution<double> dRate(0.0, 1.0);
-        for (int i = 0; i < populationSize; ++i)
+
+        for (int i = 0; i < size; ++i)
         {
             int id = dId(get);
             int point = dPoint(get);
             double rate = dRate(get);
             if (rate <= crossRate)
             {
-                allPopulation[i].cross(allPopulation[id], point);
+
+                std::pair<std::shared_ptr<individual>, std::shared_ptr<individual>> temp =
+                    currentPopulation[i]->cross(currentPopulation[id], point);
+                currentPopulation.push_back(temp.first);
+                currentPopulation.push_back(temp.second);
             }
         }
     }
@@ -154,22 +194,23 @@ public:
     void mutateOver()
     {
         std::uniform_real_distribution<double> dRate(0.0, 1.0);
-        for (int i = 0; i < populationSize; ++i)
+        size_t size = currentPopulation.size();
+        for (int i = 0; i < size; ++i)
         {
             double rate = dRate(get);
             if (rate <= mutateRate)
             {
-                allPopulation[i].mutate();
+                currentPopulation.push_back(currentPopulation[i]->mutate());
             }
         }
     }
 
-    individual getBest()
+    std::shared_ptr<individual> getBest()
     {
-        individual best = allPopulation[0];
+        std::shared_ptr<individual> best = selectedPopulation[0];
         for (int i = 1; i < populationSize; ++i)
         {
-            best = (best.fitness < allPopulation[i].fitness) ? allPopulation[i] : best;
+            best = (best->fitness < selectedPopulation[i]->fitness) ? selectedPopulation[i] : best;
         }
         return best;
     }
@@ -182,57 +223,47 @@ public:
             bool ifSame = true;
             for (int i = 0; i < populationSize - 1; ++i)
             {
-                if (allPopulation[i].x[j] != allPopulation[i + 1].x[j])
+                if (selectedPopulation[i]->x[j] != selectedPopulation[i + 1]->x[j])
                     ifSame = false;
             }
             if (ifSame)
             {
-                allPopulation[dId(get)].x[j] = !allPopulation[dId(get)].x[j];
+                std::shared_ptr<individual> temp = std::make_shared<individual>();
+                temp->x = selectedPopulation[dId(get)]->x;
+                writeFile << "Forced mutation in position " << j << " all is " << temp->x[j] << std::endl;
+                save(temp->x);
+                temp->x[j] = !temp->x[j];
+                save(temp->x);
+                currentPopulation.push_back(temp);
             }
         }
     }
 };
-void save(std::ofstream &writeFile, population &p, int Generation)
-{
-    if (writeFile)
-    {
-        writeFile << "Generation " << Generation << std::endl;
-        for (auto &i : p.allPopulation)
-        {
-            for (auto &e : i.x)
-            {
-                writeFile << e << " ";
-            }
-            writeFile << std::endl;
-        }
-        writeFile << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
-    }
-    else
-    {
-        std::cerr << "can not open writefile" << std::endl;
-    }
-}
 
 void run()
 {
+    if (!writeFile)
+    {
+        std::cerr << "can not open writefile" << std::endl;
+    }
     population p{};
-    individual best;
-    best.fitness = 0.0;
-    std::ofstream writeFile("gene.txt", std::ios::out & std::ios::trunc & std::ios::app);
     for (int i = 0; i < maxGeneration; ++i)
     {
+        writeFile << "Generation " << i + 1 << std::endl;
         p.calculateFitness();
-        auto temp = p.getBest();
-        best = (best.fitness > temp.fitness) ? best : temp;
-        std::cout << "best finess = " << temp.fitness << std::endl;
+        if (i % 100 == 0)
+        {
+            printf("Generation = %d\n", i);
+        }
+
         p.select();
         p.crossOver();
         p.mutateOver();
         p.checkAndForcedMutate();
-        save(writeFile, p, i + 1);
+        writeFile << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
     }
-    // std::cout << "best finess = " <<  best << std::endl;
-    printf("x = %.10lf, fitness = %.10lf\n", best.getDecode(), best.fitness);
+    std::shared_ptr<individual> best = p.getBest();
+    printf("Generation = %d, x = %.10lf, fitness = %.10lf\n", maxGeneration, best->getDecode(), best->fitness);
 }
 
 #endif
